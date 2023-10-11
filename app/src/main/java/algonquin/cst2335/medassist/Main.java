023443a1c54388589edebfda233259673863e699
Ethan EG <ethaneweg@gmail.com>
2023-10-11 15:19:56 -0400

    Commit 3: simple UI for main screen and android manifest setup

--- app/src/main/java/algonquin/cst2335/medassist/Main.java
+++ app/src/main/java/algonquin/cst2335/medassist/Main.java
@@ -1,2 +1,6 @@
-package algonquin.cst2335.medassist;public class Main {
+package algonquin.cst2335.medassist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
 }
